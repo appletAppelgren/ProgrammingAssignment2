d2a2f11af428fe4101b21562fded43fe0758e995
appletAppelgren <carl.o.appelgren@gmail.com>
2016-11-28 16:34:59 +0100

    commited by applet

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script returns chached inverted matrixes if it has allready been calculated, 
+## else it calculates and caches the inverted matrix
 
-## Write a short comment describing this function
+## This function takes an argument that has to be a square and invertible matrix and
+## returns a list of functions to:
+##      1 set the matrix
+##      2 get the matrix
+##      3 set the inverse
+##      4 get the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        mat <- NULL
+        set <- function(y) {
+                x <<- y
+                mat <<- NULL
+        }
+        get <- function() x
+        setinvert <- function(invert) mat <<- invert
+        getinvert <- function() mat
+        
+        list(set=set, get=get, setinvert=setinvert, getinvert=getinvert)
 
 }
 
-
-## Write a short comment describing this function
+## This function looks if we have cached the inverted matrix previously. If it has been catched the inverted
+## matrix will be returned else the function calculates the inverted matrix and caches it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        mat = x$getinvert()
+                if(!is.null(mat)){
+                        message("getting cached data")
+                        return(mat)
+                }
+        matrix <- x$get()
+        mat = solve(matrix, ...)
+        
+        x$setinvert(mat)
+        
+        return(mat)
+        
 }
+
+## just some testing of the functions...
+set.seed(1110201)
+r = rnorm(9)
+mat1 = matrix(r, nrow=3, ncol=3)
+mat2 = matrix(r, nrow=3, ncol=3)
+mat1
+mat2
+test <- makeCacheMatrix(mat1)
+test2 <- makeCacheMatrix(mat2)
+test
+test2
+cacheSolve(test)
+cacheSolve(test2)
+
